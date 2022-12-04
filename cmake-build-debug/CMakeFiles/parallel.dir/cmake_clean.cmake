70859215eb587217fed2be01d14ad5e68ecba7d7
Spica TSUKIMI <haochengvanadis@gmail.com>
2022-12-03 23:11:32 -0500

    sync main

---
+++ cmake-build-debug/CMakeFiles/parallel.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parallel.dir/ParallelMazeSolver.cpp.obj"
+  "libparallel.dll.a"
+  "parallel.exe"
+  "parallel.exe.manifest"
+  "parallel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parallel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
