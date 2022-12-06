e0e62c1e0d08c7185c570a2bf6c58b550b138159
Spica TSUKIMI <haochengvanadis@gmail.com>
2022-12-05 19:55:18 -0500

    end a* testing

--- Parallel-A-Star-Algorithm-master/spa.h
+++
@@ -1,9 +0,0 @@
-#ifndef FINAL_CENTRALIZED_H
-#define FINAL_CENTRALIZED_H
-#include "map.h"
-
-TestResult* find_path_spa(const Map* map, int);
-
-TestResult* find_path_spa_custom(const Map* map, int);
-
-#endif
